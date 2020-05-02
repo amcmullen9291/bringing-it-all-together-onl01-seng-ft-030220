6cc9fc15fa19129de827ef1d49f3946a69e3a77a
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-02 04:48:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,8 +64,14 @@ def self.new_from_db(row)
     self.new(attributes)
   end
 
-def self.find_by_id 
-  
-end
+ def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE id = ?
+    SQL
 
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
 end 
