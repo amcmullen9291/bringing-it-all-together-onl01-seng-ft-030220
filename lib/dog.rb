cfc854d1a8d4dbc1f8924877364cb9ad9d71621e
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-02 05:00:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,16 +74,6 @@ def self.new_from_db(row)
     end.first
   end
   
-  def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, album)
-    if self.id
-      self.update
-    else
-      dog = self.create(name: name, breed: breed)
-    end
-      dog
-  end
-
 def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM dogs
