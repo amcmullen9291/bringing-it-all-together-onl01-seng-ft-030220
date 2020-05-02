312c199c722978d6d88cc11e2a0f4d43cff3435c
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-02 05:08:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,7 +87,7 @@ def self.find_by_name(name)
   end
 
   def self.find_or_create_by(name:, breed:)
-    song = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?, id = ?", name, breed, id)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?, id = ?", name, breed, id)
     if !dog.empty?
     dog_data = song[0]
     dog = self.new(dog_data[0], dog_data[1], dog_data[2])
