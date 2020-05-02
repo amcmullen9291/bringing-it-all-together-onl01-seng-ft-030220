e4cd3cc211cb10c53b19192af865b6935567de5b
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-02 05:12:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -92,7 +92,7 @@ def self.find_by_name(name)
     dog_data = dog[0]
     dog = self.new(dog_data[0], dog_data[1], dog_data[2])
     else
-    song = self.create(name: name, breed: breed)
+    dog = self.create(name: name, breed: breed)
     end
 end
 
