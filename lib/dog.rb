983de27e0a95e7dae6a8eb48587901dce80d28b2
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-02 04:37:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,12 +56,12 @@ def self. new_from _db
 end 
 
 def self.new_from_db(row)
-  id = row[0]
-  name =  row[1]
-  breed = row[2]
-  new_dog = self.new(name)
-  new_dog
-end
-
+    attributes= {
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
+    }
+    self.new(attributes)
+  end
 
 end 
