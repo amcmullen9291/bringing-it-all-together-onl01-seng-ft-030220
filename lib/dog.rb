1747c6073caaefeb479c46068c8194826b1d71e4
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-02 05:14:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -94,12 +94,6 @@ def self.find_by_name(name)
     else
     dog = self.create(name: name, breed: breed)
     end
-    if dog
-        new_dog = self.new_from_db(dog)
-      else
-        new_dog = self.create({:name => name, :breed => breed})
-      end
-      new_dog
 end
 
 end 
